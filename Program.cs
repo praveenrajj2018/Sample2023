af889c9983547f490f1cc07af1a8600fa70bac9f
Praveen Raj Janarthanam <praveen.janarthanam@itp.objectfrontier.com>
2023-09-12 14:27:37 +0530

    demo2 is added

--- Program.cs
+++ Program.cs
@@ -1,3 +1,35 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("rolex");
+// C# program to demonstrate the function
+// overloading by changing the
+// Order of the parameters
+using System;
+class GFG {
+
+	// Method
+	public void Identity(String name, int id)
+	{
+
+		Console.WriteLine("Name1 : " + name + ", "
+						+ "Id1 : " + id);
+	}
+	
+	// Method
+	public void Identity(int id, String name)
+	{
+
+		Console.WriteLine("Name2 : " + name + ", "
+						+ "Id2 : " + id);
+	}
+
+	// Main Method
+	public static void Main(String[] args)
+	{
+
+		// Creating Object
+		GFG obj = new GFG();
+
+		obj.Identity("Akku", 1);
+		obj.Identity(2, "Abby");
+	}
+}
+
